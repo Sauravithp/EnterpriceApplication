437cf9aecf25708dadd98c8dd9dd3e19cae47b63
Sauravi Thapa <sauravithapa@Sauravis-MacBook-Pro.local>
2022-04-27 17:49:54 -0500

    inital commit

---
+++ Exercise03_2/src/main/java/com/example/exercise03_2/Exercise032Application.java
@@ -0,0 +1,13 @@
+package com.example.exercise03_2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Exercise032Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Exercise032Application.class, args);
+    }
+
+}
